2cf7f47353c12d233f3781d242aa6f8bce7153ac
VahramSargsyan <vahram85-85@mail.ru>
2024-03-20 14:20:41 +0400

    Написал первую и вторую задачу

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Console.Write("Введите число a: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// if (a % 7 == 0 && a % 23 == 0)
+// {
+//     Console.WriteLine("да");
+// }
+// else
+// {
+//     Console.WriteLine("нет");
+// }
+
+
+
+Console.Write("Введите координаты точки X и Y через пробел: ");
+string[] coordinates = Console.ReadLine().Split(' ');
+int x = Convert.ToInt32(coordinates[0]);
+int y = Convert.ToInt32(coordinates[1]);
+if (x > 0 && y > 0)
+    Console.WriteLine("1");
+else if (x < 0 && y > 0)
+    Console.WriteLine("2");
+else if (x < 0 && y < 0)
+    Console.WriteLine("3");
+else if (x > 0 && y < 0)
+    Console.WriteLine("4");
+else
+    Console.WriteLine("Точка находится на оси координат");
